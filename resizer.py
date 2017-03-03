fda221a42965edc188d4d7a2afa1dffc1524f1a0
Matthew Paletta <Matthew@JOE.local>
2017-03-03 15:58:00 -0800

    Initial Commit

---
+++ resizer.py
@@ -0,0 +1,9 @@
+import PIL
+from PIL import Image
+
+basewidth = 300
+img = Image.open('fullsized_image.jpg')
+wpercent = (basewidth / float(img.size[0]))
+hsize = int((float(img.size[1]) * float(wpercent)))
+img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+img.save('resized_image.jpg')
