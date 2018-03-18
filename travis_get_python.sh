77c5e922c3dc6d0cb191b202baeb90ba182eeaa9
Matthew Paletta <mattpaletta@gmail.com>
2018-03-17 22:48:24 -0700

    Add travis helper files

---
+++ travis_get_python.sh
@@ -0,0 +1,14 @@
+!/bin/sh
+if ["$TRAVIS_OS_NAME" == "osx" ]; then
+  brew update
+  brew install openssl realine
+  brew outdated pyenv || brew upgrade pyenv
+
+  brew install pyenv-virtualenv
+  pytenv install $PYTHON
+  export PYENV_VERSION=$PYTHON
+  export PATH="/Users/travis/.pyenv/shims:${PATH}"
+  pyenv-virtualenv env
+  source env/bin/activate
+  python --version
+fi
