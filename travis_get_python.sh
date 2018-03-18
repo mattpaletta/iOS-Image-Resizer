b37316de43bbe0e7c1b61f8b21b1662d1b4f4004
Matthew Paletta <mattpaletta@gmail.com>
2018-03-17 23:32:25 -0700

    Add sudo install

--- travis_get_python.sh
+++ travis_get_python.sh
@@ -6,8 +6,8 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   brew install python@{$PYTHON}
   brew link --overwrite python@{$PYTHON}
   sudo easy_install pip
-  pip install --upgrade pip setuptools
-  pip install virtualenv
+  sudo pip install --upgrade pip setuptools
+  suod pip install virtualenv
   python -m virtualenv env
   source env/bin/activate
   python --version
